d1b90e53fbe7ee66af471f03d87fd423f823b1dd
Brandon Denney <ee.brandon@gmail.com>
2022-03-14 15:53:38 -0400

    Corrected date array.

--- src/utils/dateUtils.js
+++ src/utils/dateUtils.js
@@ -1,11 +1,11 @@
 const DAYS = [
+    "Sunday",
     "Monday",
     "Tuesday",
     "Wednesday",
     "Thursday",
     "Friday",
-    "Saturday",
-    "Sunday"
+    "Saturday"
 ]
 
 function isEventAllDay(event) {
