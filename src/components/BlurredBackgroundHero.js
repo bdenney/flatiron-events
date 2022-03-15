26be0d70fa150252da7bba6ff087f5e4936ff8b5
Brandon Denney <ee.brandon@gmail.com>
2022-03-15 13:52:21 -0400

    Fixed flicker.

--- src/components/BlurredBackgroundHero.js
+++ src/components/BlurredBackgroundHero.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 
 function BlurredBackgroundHero() {
-    const NUM_OF_PHOTOS = 47; // indicies 0-46
+    const NUM_OF_PHOTOS = 46; // indicies 0-46
     const [imageIndex, setImageIndex] = useState(0);
 
     useEffect(() => {
@@ -32,9 +32,8 @@ function BlurredBackgroundHero() {
       "top": 0,
       "left":0,
       "zIndex": -2,
-      "transition": "background-image 1s ease-in-out",
-      "animation-delay": "0s",
-  
+      "transition": "background-image 2s ease-in-out",
+      "animationDelay": "0s",
       "filter": "blur(4px)",
       "WebkitFilter": "blur(4px)",
       "WebkitBackfaceVisibility": "hidden"
