4ce6e996955eb1f8b0f50ac0560dcbab15993ee2
Brandon Denney <ee.brandon@gmail.com>
2022-03-15 14:09:20 -0400

    Increasing hero transition to 15 minutes and randomizing start index.

--- src/components/BlurredBackgroundHero.js
+++ src/components/BlurredBackgroundHero.js
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from 'react'
 
 function BlurredBackgroundHero() {
     const NUM_OF_PHOTOS = 46; // indicies 0-46
-    const [imageIndex, setImageIndex] = useState(0);
+    const randomStartIndex = Math.floor(Math.random()*NUM_OF_PHOTOS);
+    const [imageIndex, setImageIndex] = useState(randomStartIndex);
+
 
     useEffect(() => {
 
@@ -14,7 +16,7 @@ function BlurredBackgroundHero() {
                 }
                 return nextIndex
             });
-        }, 60000)
+        }, 15 * 60000)
 
         return () => clearInterval(interval);
 
@@ -32,11 +34,12 @@ function BlurredBackgroundHero() {
       "top": 0,
       "left":0,
       "zIndex": -2,
-      "transition": "background-image 2s ease-in-out",
+      "transition": "background-image 1s ease-in-out",
       "animationDelay": "0s",
       "filter": "blur(4px)",
       "WebkitFilter": "blur(4px)",
-      "WebkitBackfaceVisibility": "hidden"
+      "WebkitBackfaceVisibility": "hidden",
+      "animationFillMode": "both"
     };
 
     return <div style={heroStyle}></div>
